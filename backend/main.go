6a5e55d2adb09bf8152ff5d666788069ea942d71
zhanghao123321 <65217421+zhanghao123321@users.noreply.github.com>
2025-06-25 16:32:49 +0800

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"net/http"
 	"time"
-	"nav/backend/config"
-	"nav/backend/database"
-	"nav/backend/models"
-	"nav/backend/routes"
-	"nav/backend/utils"
+	"znav/backend/config"
+	"znav/backend/database"
+	"znav/backend/models"
+	"znav/backend/routes"
+	"znav/backend/utils"
 )
 
 func init() {
